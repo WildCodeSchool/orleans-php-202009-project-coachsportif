34d14bf13bb560fdf481534f9a366831439e8da0
AliNedjar <alinedjarali45@outlook.com>
2020-12-02 17:14:31 +0100

    Initialized project

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,6 +19,7 @@ require('bootstrap');
 // require('bootstrap/js/dist/tooltip');
 // require('bootstrap/js/dist/popover');
 
-$(document).ready(function() {
+$(document).ready(() => {
     $('[data-toggle="popover"]').popover();
 });
+
