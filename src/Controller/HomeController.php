bf811ae2662eb1dc1f201e2fc068a0a771b48d62
Kronigeur <loicchenuet@hotmail.fr>
2020-12-09 15:06:52 +0100

    text acceuil BDD + fixture (changement css)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Home;
+use App\Repository\HomeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,9 +12,13 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @param HomeRepository $homeRepository
+     * @return Response
      */
-    public function index(): Response
+    public function index(HomeRepository $homeRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        $home = $homeRepository->findAll();
+        return $this->render('home/index.html.twig', ['home' => $home]);
     }
+
 }
