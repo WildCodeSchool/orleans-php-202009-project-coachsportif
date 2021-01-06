932445c8edd02128c5d5da6145784ce370ffefff
Kronigeur <loicchenuet@hotmail.fr>
2021-01-06 14:25:10 +0100

    création Sport adapte BDD

--- migrations/Version20210106132408.php
+++ migrations/Version20210106132408.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210106093916 extends AbstractMigration
+final class Version20210106132408 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,16 +20,14 @@ final class Version20210106093916 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE sport_adapte (id INT AUTO_INCREMENT NOT NULL, description LONGTEXT NOT NULL, picture VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('DROP TABLE carousel');
-        $this->addSql('ALTER TABLE fitness ADD fitness_text LONGTEXT NOT NULL, DROP fitness_text_1, DROP fitness_text_2');
-        $this->addSql('ALTER TABLE home DROP picture');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE carousel (id INT AUTO_INCREMENT NOT NULL, path VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, page VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE fitness ADD fitness_text_2 LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE fitness_text fitness_text_1 LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
-        $this->addSql('ALTER TABLE home ADD picture LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('DROP TABLE sport_adapte');
     }
 }
