8d90d23e1da50ce43bd45a28eb007f659d228dfe
BenjaminDbse <benjideboise@gmail.com>
2021-01-07 11:27:56 +0100

    edit tarif

--- src/Controller/AdminTariffController.php
+++ src/Controller/AdminTariffController.php
@@ -48,4 +48,28 @@ class AdminTariffController extends AbstractController
         }
         return $this->render('admin/tarif/new.html.twig', ["form" => $form->createView()]);
     }
+
+    /**
+     * @Route("/{id}/edit", name="edit", methods={"GET","POST"})
+     * @param Request $request
+     * @param Tariff $tarif
+     * @return Response
+     */
+    public function edit(Request $request, tariff $tarif): Response
+    {
+        $form = $this->createForm(TariffType::class, $tarif);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+            $this->addFlash('success', 'Le tarif à bien été modifier');
+
+
+            return $this->redirectToRoute('tarif_index');
+        }
+        return $this->render('admin/tarif/edit.html.twig', [
+            'tarif' => $tarif,
+            'form' => $form->createView(),
+        ]);
+    }
 }
