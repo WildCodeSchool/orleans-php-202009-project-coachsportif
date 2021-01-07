67aeaf52ab5d7eb9521335c45a419cc2485443d9
AliNedjar <alinedjarali45@outlook.com>
2021-01-07 10:03:12 +0100

    fix

--- migrations/Version20210107090056.php
+++ migrations/Version20210107090056.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210106093800 extends AbstractMigration
+final class Version20210107090056 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,7 +20,7 @@ final class Version20210106093800 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE walking ADD updated_at DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE walking ADD updated_at DATETIME NOT NULL');
     }
 
     public function down(Schema $schema) : void
