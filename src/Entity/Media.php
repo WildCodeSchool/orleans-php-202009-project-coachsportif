96a4b722a19928c5a3bac4952166bf4e3b52b0d8
Kronigeur <loicchenuet@hotmail.fr>
2021-01-12 17:07:42 +0100

    création bdd

---
+++ src/Entity/Media.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MediaRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MediaRepository::class)
+ */
+class Media
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private ?int $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private ?string $idYoutube;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $title;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdYoutube(): ?string
+    {
+        return $this->idYoutube;
+    }
+
+    public function setIdYoutube(string $idYoutube): self
+    {
+        $this->idYoutube = $idYoutube;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+}
