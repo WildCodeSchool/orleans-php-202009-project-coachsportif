a629a642dc42e94df683b1171fc1853be5b19438
Kronigeur <loicchenuet@hotmail.fr>
2021-01-13 08:58:26 +0100

    add annotation

--- src/Controller/MediaController.php
+++ src/Controller/MediaController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\MediaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation;
 
 /**
  * @Route("/media")
