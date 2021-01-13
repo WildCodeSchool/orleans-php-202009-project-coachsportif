0090d0190c08fa88a05eed0ec2eb5b410842bcee
Kronigeur <loicchenuet@hotmail.fr>
2021-01-13 09:17:27 +0100

    erreur annotation

--- src/Controller/MediaController.php
+++ src/Controller/MediaController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use App\Repository\MediaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/media")
