7447806926aecc089ffddc2a3141b996c008010c
Pil <70975556+arnaudpilato@users.noreply.github.com>
2021-01-20 14:41:10 +0100

    add admin walking controller and change routes

--- migrations/Version20210120132613.php
+++ migrations/Version20210120132613.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210120103016 extends AbstractMigration
+final class Version20210120132613 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -21,11 +21,19 @@ final class Version20210120103016 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE company (id INT AUTO_INCREMENT NOT NULL, description LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE home CHANGE updated_at updated_at DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE opinion ADD page VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE sport_adapte DROP picture');
+        $this->addSql('ALTER TABLE walking CHANGE updated_at updated_at DATETIME DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE company');
+        $this->addSql('ALTER TABLE home CHANGE updated_at updated_at DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE opinion DROP page');
+        $this->addSql('ALTER TABLE sport_adapte ADD picture VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE walking CHANGE updated_at updated_at DATETIME DEFAULT NULL');
     }
 }
