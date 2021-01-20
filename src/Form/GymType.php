61f4bb762a6b11704bbfe7f23492de363cf666f3
AliNedjar <alinedjarali45@outlook.com>
2021-01-20 15:08:26 +0100

    added label

--- src/Form/GymType.php
+++ src/Form/GymType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Gym;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,9 @@ class GymType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('gymText')
+            ->add('gymText', TextareaType::class, [
+                'label' => 'Nouveau Texte :'
+            ])
         ;
     }
 
