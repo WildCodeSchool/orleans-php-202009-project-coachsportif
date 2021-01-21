745127bcb21f90b220d638a1278ae09f2b8c6ef6
Kronigeur <loicchenuet@hotmail.fr>
2021-01-21 15:05:52 +0100

    upload attente PR

--- src/Entity/Presentation.php
+++ src/Entity/Presentation.php
@@ -4,6 +4,12 @@ namespace App\Entity;
 
 use App\Repository\PresentationRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use DateTime;
 
 /**
  * @ORM\Entity(repositoryClass=PresentationRepository::class)
@@ -26,6 +32,44 @@ class Presentation
      * @ORM\Column(type="text")
      */
     private ?string $path;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private ?\DateTimeInterface $updatedAt;
+
+    /**
+     * @Vich\UploadableField(mapping="presentation_file", fileNameProperty="path")
+     * @var File|null
+     * @Assert\File(
+     *     maxSize="1000000",
+     *     mimeTypes = {"image/png", "image/jpeg", "image/jpg", "image/gif",})
+     */
+    private ?File $pathFile = null;
+
+    public function setPathFile(?File $image = null): Presentation
+    {
+        $this->pathFile = $image;
+        if ($image) {
+            $this->updatedAt = new DateTime('now');
+        }
+        return $this;
+    }
+
+    public function getPathFile(): ?File
+    {
+        return $this->pathFile;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
 
     public function getId(): ?int
     {
---
+++ src/Form/PresentationType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Presentation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichFileType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class PresentationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('description')
+            ->add('pathFile', VichFileType::class, [
+                'required' => false,
+                'allow_delete' => true, // not mandatory, default is true
+                'download_uri' => true, // not mandatory, default is true
+                'label' => 'Image à télécharger :'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Presentation::class,
+        ]);
+    }
+}
