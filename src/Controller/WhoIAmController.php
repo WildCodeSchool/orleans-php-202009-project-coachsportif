e5224bb23f6428c155941f4b8e79abe2f85a2015
Kronigeur <loicchenuet@hotmail.fr>
2021-01-21 10:06:25 +0100

    creation entity fixture et vue mise a jour

--- src/Controller/WhoIAmController.php
+++ src/Controller/WhoIAmController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Form\OpinionType;
 use App\Repository\OpinionRepository;
+use App\Repository\PresentationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,12 +17,16 @@ class WhoIAmController extends AbstractController
     /**
      * @Route("/", name="who")
      * @param OpinionRepository $opinionRepository
+     * @param PresentationRepository $presentations
      * @return Response
      */
-    public function index(OpinionRepository $opinionRepository): Response
-    {
+    public function index(
+        OpinionRepository $opinionRepository,
+        PresentationRepository $presentations
+    ): Response {
         $opinions = $opinionRepository->findBy(['page' => OpinionType::WHO_PAGE,]);
         return $this->render('whoIAm/index.html.twig', [
+            'presentations' => $presentations->findAll(),
             'opinions' => $opinions
         ]);
     }
