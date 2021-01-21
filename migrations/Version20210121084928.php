fc984094b36557aaf7459ef83750a6b890b1aa80
Sandra-Louchart <sandra.louchart@gmail.com>
2021-01-21 11:38:37 +0100

    US48 View client

---
+++ migrations/Version20210121084928.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210121084928 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE activity (id INT AUTO_INCREMENT NOT NULL, description LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE company (id INT AUTO_INCREMENT NOT NULL, description LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE home CHANGE picture picture VARCHAR(255) NOT NULL, CHANGE updated_at updated_at DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE opinion ADD page VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE sport_adapte DROP picture');
+        $this->addSql('ALTER TABLE user ADD firstname VARCHAR(255) DEFAULT NULL, ADD lastname VARCHAR(255) DEFAULT NULL, ADD age INT DEFAULT NULL, ADD size INT DEFAULT NULL, ADD weight INT DEFAULT NULL, ADD fat_mass INT DEFAULT NULL, ADD body_water INT DEFAULT NULL, ADD muscle_mass INT DEFAULT NULL, ADD arm_circumference INT DEFAULT NULL, ADD waist_size INT DEFAULT NULL, ADD thigh_circumference INT DEFAULT NULL, ADD ruffier_dickston_test INT DEFAULT NULL, ADD three INT DEFAULT NULL, ADD pathology VARCHAR(255) DEFAULT NULL, ADD treatment VARCHAR(255) DEFAULT NULL, ADD prescription VARCHAR(255) DEFAULT NULL, ADD imc INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE walking ADD updated_at DATETIME NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE activity');
+        $this->addSql('DROP TABLE company');
+        $this->addSql('ALTER TABLE home CHANGE picture picture VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`, CHANGE updated_at updated_at DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE opinion DROP page');
+        $this->addSql('ALTER TABLE sport_adapte ADD picture VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE user DROP firstname, DROP lastname, DROP age, DROP size, DROP weight, DROP fat_mass, DROP body_water, DROP muscle_mass, DROP arm_circumference, DROP waist_size, DROP thigh_circumference, DROP ruffier_dickston_test, DROP three, DROP pathology, DROP treatment, DROP prescription, DROP imc');
+        $this->addSql('ALTER TABLE walking DROP updated_at');
+    }
+}
---
+++ src/DataFixtures/ActivityFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Activity;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+
+class ActivityFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $activity = new Activity();
+        $activity->setDescription("Ok, le sport c'est bon pour la santé, mais comment me motiver et 
+            trouver la fréquence et les exercices adaptés à ma situation ? Avec Coach Damien, c'est 100% sur-mesure,
+            avec des exercices adaptés à votre morphologie et à votre état de santé (sédentaire, pathologie ALD...).
+            Un travail en duo, objectif bien-être et plaisir de prendre soin de son corps. 
+            Des exercices graduels pour progresser en fonction de vos besoins et de vos attentes : 
+            cardio, renforcement musculaire, souplesse, coordination, équilibre, avec ou sans matériel. 
+            Une à 3 fois par semaine, pendant 1h ,nous pouvons travailler chez vous, dans ma salle privée, en vision, 
+            en plein air... En cette période de pandémie, ma salle reste accessible aux personnes ayant une 
+            prescription médicale pour la pratique de l'activité physique. On commence ?");
+        $manager->persist($activity);
+        $manager->flush();
+    }
+}
