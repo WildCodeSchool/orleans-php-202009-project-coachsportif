73b8b578bf3f21286610d66132ee56dfbabcb30d
Kronigeur <loicchenuet@hotmail.fr>
2021-02-08 09:29:54 +0100

    test

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -37,12 +37,16 @@ class HomeController extends AbstractController
         CarouselRepository $carouselRepository
     ): Response {
         $contact = new ContactHome();
+        /** @var string $from */
+        $from = $this->getParameter('mailer_from');
+        /** @var string $mailTo */
+        $mailTo = $this->getParameter('mailer_from');
         $form = $this->createForm(ContactHomeType::class, $contact);
         $form->handleRequest($request);
         if (($form->isSubmitted() && $form->isValid())) {
             $email = (new Email())
-                ->from($this->getParameter('mailer_from'))
-                ->to($this->getParameter('mailer_to'))
+                ->from($from)
+                ->to($mailTo)
                 ->subject('Sujet:' . $contact->getSubject())
                 ->html($this->renderView('home/contactHomeEmail.html.twig', ['contact' => $contact]));
             $mailer->send($email);
